2c55dc3c92a13b0063bd86f5dce5e77a828b1c09
Stefan Caras <stefan.caras@gmail.com>
2022-09-09 15:06:17 +0300

    updates

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-let title = document.querySelector('#title');
-let author = document.querySelector('#author');
-let isbn = document.querySelector('#isbn');
-let button = document.querySelector('button');
-let table = document.querySelector('table');
-
-isbn.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      button.click();
-    }
-});
-
-button.addEventListener('click', Ul.add());
-table.addEventListener('click', Ul.remove());
-
-class Book{
+class Book {
 	constructor(title, author, isbn){
 		this.title = title;
 		this.author = author;
@@ -21,32 +6,51 @@ class Book{
 	}
 }
 
-class Ul{
-	constructor(){
+class UI {
+	static add(book) {
+		console.log("helloooo")
+		let row = table.insertRow(1);
+		let cell1 = row.insertCell(0);
+		let cell2 = row.insertCell(1);
+		let cell3 = row.insertCell(2);
+		let cell4 = row.insertCell(3);
+		cell1.innerText = book.title;
+		cell2.innerText = book.author;
+		cell3.innerText = book.isbn;
+		cell4.innerHTML = `<i class="fas fa-window-close" id="x"></i>`;
 	}
-	static add() {
-		let book1 = new Book("Catch22", "Joseph Heller", 5325432423)
-		console.log(book1)
-		if (title.value != "" && author.value != "" && isbn.value != "") {
-			let row = table.insertRow(1);
-			let cell1 = row.insertCell(0);
-			let cell2 = row.insertCell(1);
-			let cell3 = row.insertCell(2);
-			let cell4 = row.insertCell(3);
-			cell1.innerText = title.value;
-			cell2.innerText = author.value;
-			cell3.innerText = isbn.value;
-			cell4.innerHTML = `<i class="fas fa-window-close" id="x"></i>`;
-			title.value = "";
-			author.value = "";
-			isbn.value = "";
+	static remove(el) {
+		if (el.id == 'x') {
+			el.parentNode.parentNode.remove();
 		}
 	}
-	static remove(event) {
-		const clickedElement = event.target;
-		const td1 = clickedElement.parentNode.parentNode.firstChild;
-		if (clickedElement.id == 'x') {
-			clickedElement.parentNode.parentNode.remove();
-		}
-	}
+	static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
 }
+
+let button = document.querySelector('button');
+let table = document.querySelector('table');
+
+button.addEventListener('click', () => {
+	const title = document.querySelector('#title').value;
+	const author = document.querySelector('#author').value;
+	const isbn = document.querySelector('#isbn').value;
+	if (title != "" && author != "" && isbn != "") {
+		const book = new Book(title, author, isbn);
+		UI.add(book);
+		UI.clearFields();
+	}
+});
+
+table.addEventListener('click', (e) => {
+	UI.remove(e.target);
+});
+
+isbn.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      button.click();
+    }
+});
