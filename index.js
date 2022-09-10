5bc385296319c794fd6efcda2982d8a7dbb88583
Stefan Caras <stefan.caras@gmail.com>
2022-09-10 08:46:45 +0300

    updates

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const button = document.querySelector('button');
+const table = document.querySelector('table');
+
 class Book {
 	constructor(title, author, isbn){
 		this.title = title;
 		this.author = author;
 		this.isbn = isbn;
 	}
-}
+};
 
 class UI {
 	static createBook() {
@@ -21,11 +24,11 @@ class UI {
 		}
 	}
 	static add(book) {
-		let row = table.insertRow(1);
-		let cell1 = row.insertCell(0);
-		let cell2 = row.insertCell(1);
-		let cell3 = row.insertCell(2);
-		let cell4 = row.insertCell(3);
+		const row = table.insertRow(1);
+		const cell1 = row.insertCell(0);
+		const cell2 = row.insertCell(1);
+		const cell3 = row.insertCell(2);
+		const cell4 = row.insertCell(3);
 		cell1.innerText = book.title;
 		cell2.innerText = book.author;
 		cell3.innerText = book.isbn;
@@ -37,13 +40,14 @@ class UI {
 		}
 	}
 	static clearFields() {
-        document.querySelector('#title').value = '';
-        document.querySelector('#author').value = '';
-        document.querySelector('#isbn').value = '';
-    }
+		document.querySelector('#title').value = '';
+		document.querySelector('#author').value = '';
+		document.querySelector('#isbn').value = '';
+	}
 	static message(msg) {
 		const form = document.querySelector('#form');
 		const p = document.createElement('p');
+		p.classList.add('msg');
 		if (msg === 'confirm') {
 			p.innerText = 'Cartea a fost adăugată.'
 		} else {
@@ -51,20 +55,19 @@ class UI {
 			p.classList.add('error');
 		}
 		form.appendChild(p);
-		//remove message/error after 2 seconds
-		const paragraph = document.querySelector('p');
-		setTimeout(()=>paragraph.remove(),2000);
+		//remove message after 2 seconds
+		const ps = document.querySelectorAll('.msg');
+		setTimeout(() => {for (let el of ps){el.remove()}}, 2000);
 	}
 	static clickButtonOnEnter(event) {
 		if (event === 'Enter') {
 			button.click();
 		}
 	}
-}
-
-let button = document.querySelector('button');
-let table = document.querySelector('table');
-
-button.addEventListener('click', () => UI.createBook());
-table.addEventListener('click', (e) => UI.remove(e.target));
-isbn.addEventListener('keypress', (e) => UI.clickButtonOnEnter(e.key));
+	static eventListeners() {
+		button.addEventListener('click', () => UI.createBook());
+		table.addEventListener('click', (e) => UI.remove(e.target));
+		isbn.addEventListener('keypress', (e) => UI.clickButtonOnEnter(e.key));
+	}
+};
+UI.eventListeners();
